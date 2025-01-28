ecc129cf33f39063707048899abf06d9a32978d6
Mykola Gurov <ngurov@gmail.com>
2025-01-28 09:23:32 +0100

    tutorial step 1 done

--- tests/app-tests/tutorial/01-contact-route.spec.ts
+++ tests/app-tests/tutorial/01-contact-route.spec.ts
@@ -10,5 +10,6 @@ test('can open contact', async ({ page }) => {
   await page.goto('/contacts/1');
 
   await expect(page.getByText("React Router Contacts")).toBeVisible();
+  await expect(page.getByAltText("Your Name avatar")).toBeVisible();
 });
 
